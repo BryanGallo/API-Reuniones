06459c91ff530c07ce49194f16c13016aaa06369
BryanGallo <bryangallo95@hotmail.com>
2026-01-02 21:53:10 -0500

    feat: agregando colors para logs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { app } from "./server.js";
+import colors from "colors";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(colors.green(`🚀 Server is running on port ${PORT}`));
 });
