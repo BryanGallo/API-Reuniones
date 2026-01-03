b55183b4ac5e74058900d7fc30695ed90c333c96
BryanGallo <bryangallo95@hotmail.com>
2026-01-02 22:05:52 -0500

    feat: agregando midddleware json - agregando ruta y error de rutas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import { loginRoutes } from "./routes/loginRoutes.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(express.static("./src/public"));
+
+app.use("/api", loginRoutes);
+
+// Middleware para manejar rutas no encontradas (404)
+app.use((req, res) => {
+    res.status(404).json({
+        error: "Ruta no encontrada",
+        message: `La ruta ${req.method} ${req.path} no existe en el servidor`,
+        path: req.path,
+    });
+});
 
 export { app };
